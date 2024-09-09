7485e0ea314e2c153004248f23da2d488d98ba10
Flávia Correia <fllaviacorreia@outlook.com>
2024-09-09 16:51:57 -0300

    create details screen

---
+++ DetailsScreen.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RoutesParamList } from './AppNavigation';
+import { useNavigation } from '@react-navigation/native';
+
+type detailsScreenProp = NativeStackNavigationProp<RoutesParamList, "Details">;
+
+export default function DetailsScreen() {
+    const navigation = useNavigation<detailsScreenProp>();
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+    );
+  }
